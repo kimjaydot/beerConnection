68d4ef95a92bb53f6ff0bde6a876aa64bd88a14e
Kimberley Johnson <kimberleyjohnson@Kimberleys-MacBook-Pro.local>
2017-04-20 14:39:52 -0700

    First layout of Beer Connection

---
+++ js/app.js
@@ -0,0 +1,15 @@
+// Beer Connection External JavaScript File
+
+   var config = {
+      apiKey: "Whatever API",
+      authDomain: "anything.firebaseapp.com",
+      databaseURL: "https://anything.firebaseio.com/",
+      storageBucket: "anything.appspot.com"
+    };
+    firebase.initializeApp(config);
+
+
+// Create a variable to reference the database
+
+	var database = firebase.database();
+
